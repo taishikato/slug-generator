4cb5576b472708e264460bee00df1a7190fd748e
taishikato <taishi.k0903@gmail.com>
2020-04-05 11:58:25 -0700

    :white_check_mark: Mock uuid library for better test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,7 @@
 import Index from '../'
+import { v4 as uuidv4 } from 'uuid'
+import { mocked } from 'ts-jest/utils'
+jest.mock('uuid')
 
 test('Return an empty string when passed an empty string', () => {
   const target = ''
@@ -25,7 +28,9 @@ test('Delete special characters', () => {
 })
 
 test('Generate a slug with UUID', () => {
+  // Set a return value of the mock for uuidv4
+  mocked(uuidv4).mockReturnValueOnce('suffix')
   const target = 'blog title (March 1st)'
-  const expected = /blog-title-march-1st-.*/
+  const expected = 'blog-title-march-1st-suffix'
   expect(Index(target, true)).toMatch(expected)
 })
